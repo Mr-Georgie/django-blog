4cb2b12f3a1b4bfb24747d96fc9b41d5fd4ec3c0
supergizzy <george.isiguzo@yahoo.com>
2021-04-28 06:07:37 +0100

    not done yet

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import django_heroku
+django_heroku.settings(locals())
